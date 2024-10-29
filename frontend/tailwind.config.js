baf551a62ac55edeced1928aef36214c6ea0cf22
Csongor Bőjte <94720346+CsongiBojte@users.noreply.github.com>
2024-10-29 09:08:02 +0200

    chore: setup typography, fonts, color palette, icons (#16)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#EBE8E7',
+        secondary: '#136A5C',
+        neutrals: {
+          900: '#171718',
+          800: '#212122',
+          700: '#4E4E51',
+        },
+      },
+    },
   },
   plugins: [],
 };
