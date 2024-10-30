9989c67f49601758a16dad370a48082077a7a3c0
Csongor Bőjte <94720346+CsongiBojte@users.noreply.github.com>
2024-10-30 13:17:15 +0200

    feat: create navigation menu (#18)
    * Add NavigationMenu
    
    * Add hover state style + refactor
    
    * Fix NavLinkAtom import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { LandingPage } from './pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage></LandingPage>,
+  },
+]);
+
 export default function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-blue-600">Hello world!</h1>
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ frontend/src/components/NavigationMenu/NavigationMenu.tsx
@@ -0,0 +1,24 @@
+import { NavLinkAtom } from '../../atoms';
+import { NAV_LINKS } from '../../utils';
+
+export const NavigationMenu = () => {
+  return (
+    <header className='flex justify-center'>
+      <nav className='flex flex-col gap-7 justify-center items-center'>
+        <h1 className='font-bold text-2xl justify-self-center'>BionicScolar</h1>
+        <ul className='flex gap-5 items-center justify-center'>
+          {NAV_LINKS.map((navLink, index) => {
+            return (
+              <li key={index}>
+                <NavLinkAtom to={navLink.to} label={navLink.name} />
+              </li>
+            );
+          })}
+          <li>
+            <NavLinkAtom to='/upload' label='Upload Research Paper' className='border border-neutral-900' />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
