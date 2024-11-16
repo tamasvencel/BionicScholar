d610c44cbac90d3265526c39264a602c4bc56f7c
Tamas Vencel <99116208+tamasvencel@users.noreply.github.com>
2024-11-16 14:09:10 +0200

    Extract text from images if PDF is not readable (#42)

--- backend/app/research_paper_analyzer.py
+++ backend/app/research_paper_analyzer.py
@@ -2,6 +2,9 @@ import os
 from django.conf import settings
 import shutil
 import fitz
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from PIL import Image
+import pytesseract
 
 from langchain_huggingface import HuggingFaceEndpoint
 from langchain_community.document_loaders import PyPDFLoader
@@ -21,13 +24,17 @@ class AnalyzeResearchPaper:
     def __init__(self, filename):
         self.research_paper_file_path = f"research_papers/{filename}.pdf"
 
-    def generate_pdf(self):
+    def analyzePDF(self):
         """
         Main method to generate the analyzed pdf.
         """
 
-        pdf_image_paths = self.__load_pdf(file_path=self.research_paper_file_path)
-        print(pdf_image_paths)
+        # Load and process PDF
+        pdf_text_or_image_paths, pdf_isReadable = self.__load_pdf(file_path=self.research_paper_file_path)
+        
+        pdf_text = self.__extract_text_from_images(image_paths=pdf_text_or_image_paths) if not pdf_isReadable else pdf_text_or_image_paths
+        
+        print(pdf_text)
 
     def __load_pdf(self, file_path):
         """
@@ -36,7 +43,18 @@ class AnalyzeResearchPaper:
 
         input_file_path = f"{settings.MEDIA_ROOT}/{file_path}"
         
-        # convert pdf pages to images
+        # Check if the PDF is readable or scanned
+        loader = PyPDFLoader(input_file_path)
+        pdf = loader.load_and_split()
+        
+        if pdf:
+            isReadable = True
+            
+            pdf_text = "\n".join([page.page_content for page in pdf])
+            
+            return pdf_text, isReadable
+        
+        # If the pdf is not readable, convert its pages to images
         output_dir = f"{settings.MEDIA_ROOT}/temp_images/"
 
         # remove output_dir folder and its content if it already exists
@@ -58,7 +76,35 @@ class AnalyzeResearchPaper:
             image_paths.append(image_filename)
 
         doc.close()
-        return image_paths
+        return image_paths, False
+    
+    def __extract_text_from_images(self, image_paths):
+        """
+        Distribute the text extraction to several workers for parallel processing.
+        """
+        extracted_texts = [""] * len(image_paths)
+        
+        # Use ThreadPoolExecutor for concurrent extraction
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [executor.submit(self.__extract_text_from_a_single_image, data, idx) for idx, data in enumerate(image_paths)]
+            
+            for future in as_completed(futures):
+                text_from_image, image_idx = future.result()
+                
+                extracted_texts[image_idx] = text_from_image
+                
+        return "".join([extracted_text for extracted_text in extracted_texts])
+    
+    def __extract_text_from_a_single_image(self, image_path, image_idx):
+        """
+        Extract text from a single image using PaddleOCR.
+        """
+
+        image = Image.open(image_path)
+        
+        text_from_image = pytesseract.image_to_string(image)
+        
+        return text_from_image, image_idx
 
     # def __extract_info(self):
     #     messages = [
