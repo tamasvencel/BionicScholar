aa390507978b7cd036fb805c54c3ef145f72c4e4
Tamas Vencel <tamasvenci46@gmail.com>
2024-11-25 14:44:16 +0200

    Initial docker configurations

--- backend/api/settings.py
+++ backend/api/settings.py
@@ -16,8 +16,13 @@ import os
 import platform
 import pytesseract
 
-# Load env variables
-load_dotenv()
+# Check environment
+environment = os.getenv('ENVIRONMENT', 'development')
+
+if environment == 'development':
+    load_dotenv('.env.dev')  # Load development env vars
+elif environment == 'production':
+    load_dotenv('.env.prod')  # Load production env vars
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
