5da6ce44e9e15c9dd796a4767f36e67f3e0f3593
Csongor Bőjte <bojte.csongi12@gmail.com>
2024-11-27 13:40:24 +0200

    Dockerize backend, frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,14 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
-  preview: {
-    port: 80,
-    strictPort: true,
-  },
   server: {
-    port: 80,
+    port: 3000,
     strictPort: true,
     host: true,
-    origin: 'http://localhost:80',
   },
 });
