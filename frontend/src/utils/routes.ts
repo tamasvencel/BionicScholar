fe34d79ec2f33fc199ee9e4cd4fd927a8ec2022d
TamasVencel3 <tamasvenci46@gmail.com>
2024-11-28 11:24:40 +0200

    Set routes on frontend based on production or development environment

--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -1,8 +1,15 @@
 // routes.ts
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const API_BASE_URL = isProduction
+  ? 'https://bionicscholar-fubjedg4gcgzd6dz.germanywestcentral-01.azurewebsites.net'
+  : 'http://127.0.0.1:8000';
 
 export const ROUTES = {
   UPLOAD: `${API_BASE_URL}/api/upload/`,
-  WEBSOCKET: (fileName: string) => `ws://127.0.0.1:8000/ws/research_papers/${fileName}/`,
+  WEBSOCKET: (fileName: string) =>
+    `ws://${
+      isProduction ? 'bionicscholar-fubjedg4gcgzd6dz.germanywestcentral-01.azurewebsites.net' : '127.0.0.1:8000'
+    }/ws/research_papers/${fileName}/`,
   PDF_URL: (relativePath: string) => `${API_BASE_URL}${relativePath}`,
 };
