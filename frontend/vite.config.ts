f5751318dbce3d101e196c90365f8394f1e217f8
TamasVencel3 <tamasvenci46@gmail.com>
2024-11-28 18:44:10 +0200

    Deploy app on azure

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: 'dist',
+  },
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 80,
     strictPort: true,
     host: true,
   },
