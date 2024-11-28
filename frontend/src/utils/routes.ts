bccd2fbfb786451edfe8a03f8699fa9921947f3c
TamasVencel3 <tamasvenci46@gmail.com>
2024-11-28 14:07:28 +0200

    remove nginx, not necessary for azure deployment

--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -3,13 +3,13 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 export const API_BASE_URL = isProduction
   ? 'https://bionicscholar-fubjedg4gcgzd6dz.germanywestcentral-01.azurewebsites.net'
-  : 'http://127.0.0.1:8000';
+  : 'http://127.0.0.1:80';
 
 export const ROUTES = {
   UPLOAD: `${API_BASE_URL}/api/upload/`,
   WEBSOCKET: (fileName: string) =>
     `ws://${
-      isProduction ? 'bionicscholar-fubjedg4gcgzd6dz.germanywestcentral-01.azurewebsites.net' : '127.0.0.1:8000'
+      isProduction ? 'bionicscholar-fubjedg4gcgzd6dz.germanywestcentral-01.azurewebsites.net' : '127.0.0.1:80'
     }/ws/research_papers/${fileName}/`,
   PDF_URL: (relativePath: string) => `${API_BASE_URL}${relativePath}`,
 };
