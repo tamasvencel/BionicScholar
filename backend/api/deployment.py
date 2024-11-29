98d325475a1c833b2232de538a2b4e51d2317791
Csongor Bőjte <94720346+CsongiBojte@users.noreply.github.com>
2024-11-29 12:57:18 +0200

    feat: deploy app on azure (#62)
    * Set routes on frontend based on production or development environment
    
    * Add frontend url to CORS_ALLOWED_ORIGINS for deployment
    
    * remove nginx, not necessary for azure deployment
    
    * Deploy app on azure
    
    * frontend successfully deployed
    
    * need to fix CORS
    
    * set CORS_ALLOW_CREDENTIALS to True and fix re_path missing include
    
    * improve prompt for acquiring the title from the document
    
    ---------
    
    Co-authored-by: TamasVencel3 <tamasvenci46@gmail.com>

--- backend/api/deployment.py
+++ backend/api/deployment.py
@@ -2,8 +2,8 @@ import os
 from .settings import *
 from .settings import BASE_DIR
 
-ALLOWED_HOSTS = [os.environ["WEBSITE_HOSTNAME"]]
-CSRF_TRUSTED_ORIGINS = ["https://" + os.environ["WEBSITE_HOSTNAME"]]
+ALLOWED_HOSTS = [os.environ.get("WEBSITE_HOSTNAME")]
+CSRF_TRUSTED_ORIGINS = ["https://" + os.environ.get("WEBSITE_HOSTNAME")]
 DEBUG = False
 CORS_ALLOW_ALL_ORIGINS = False
 
@@ -20,7 +20,10 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware"
 ]
 
-#CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    "https://bionicscholar-frontend-arfrffe7f3c6b8fz.germanywestcentral-01.azurewebsites.net",
+    f"https://{os.environ.get("WEBSITE_HOSTNAME")}"
+]
 
 STORAGES = {
     "default": {
@@ -30,3 +33,7 @@ STORAGES = {
         "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
+
+CORS_ALLOW_CREDENTIALS = True
+
+SERVE_MEDIA = True
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +28,8 @@ urlpatterns = [
 
 if settings.DEBUG == 1:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
+if settings.DEBUG == 0:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
