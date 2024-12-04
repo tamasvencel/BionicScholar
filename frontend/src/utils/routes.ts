95c752911f079970ef1f0df5a75cde0a16f8d4ab
TamasVencel3 <tamasvenci46@gmail.com>
2024-12-04 20:21:38 +0200

    fix: Azure App Service suddenly stops working so need to create a new App Service which fixes the problem

--- frontend/src/utils/routes.ts
+++ frontend/src/utils/routes.ts
@@ -2,14 +2,14 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 export const API_BASE_URL = isProduction
-  ? 'https://bionicscholar-fubjedg4gcgzd6dz.germanywestcentral-01.azurewebsites.net'
+  ? 'bionicscholar-backend-c2bye0gxh9a9afgm.germanywestcentral-01.azurewebsites.net'
   : 'http://127.0.0.1:80';
 
 export const ROUTES = {
   UPLOAD: `${API_BASE_URL}/api/upload/`,
   WEBSOCKET: (fileName: string) =>
     `${isProduction ? 'wss' : 'ws'}://${
-      isProduction ? 'bionicscholar-fubjedg4gcgzd6dz.germanywestcentral-01.azurewebsites.net' : '127.0.0.1:80'
+      isProduction ? 'bionicscholar-backend-c2bye0gxh9a9afgm.germanywestcentral-01.azurewebsites.net' : '127.0.0.1:80'
     }/ws/research_papers/${fileName}/`,
   PDF_URL: (relativePath: string) => `${API_BASE_URL}${relativePath}`,
 };
